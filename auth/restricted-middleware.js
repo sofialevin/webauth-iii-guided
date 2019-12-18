96c564f515dd58fe615dfc14c660d4ace1578b29
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-18 13:34:26 -0800

    Configured jwt in restricted-middleware.

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,21 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const { authorization } = req.headers;
+  
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || "shhhhh!";
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        req.token = decodedToken;
+
+        next();
+      }
+    })
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "Please login and try again" })
   }
 };
